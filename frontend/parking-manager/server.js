8d88f85ec7fd4972545c409123f38cc42e25438c
andrecigarro <andrecigarro@hotmail.com>
2026-02-04 12:14:35 +0000

    feat: Add middleware to disable Content Security Policy for all sources

--- frontend/parking-manager/server.js
+++ frontend/parking-manager/server.js
@@ -4,6 +4,14 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+// Disable CSP entirely (allow all sources)
+app.use((req, res, next) => {
+  res.removeHeader('Content-Security-Policy');
+  res.removeHeader('X-Content-Security-Policy');
+  res.setHeader('Content-Security-Policy', "default-src * 'unsafe-inline' 'unsafe-eval' data: blob:;");
+  next();
+});
+
 // Serve static files from the build directory
 app.use(express.static(path.join(__dirname, 'build')));
 
